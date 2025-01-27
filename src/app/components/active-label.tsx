e42f339a90a22a69c2fcfdc68b3a16edbea06b26
Mariya Buzhava <bujava.masha@gmail.com>
2025-01-27 23:04:44 +0200

    add css active, not-active label

--- src/app/components/active-label.tsx
+++ src/app/components/active-label.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './active-label.module.css';
 
 export interface ActiveLabelProps {
   children: React.ReactNode;
 }
 
 export default function ActiveLabel({ children }: ActiveLabelProps) {
-  return <span>{children}</span>;
+  return <span className={styles.label}>{children}</span>;
 }
