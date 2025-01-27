21c8f76c0ec29610024c85b3f4ed97c51a98fdf2
Mariya Buzhava <bujava.masha@gmail.com>
2025-01-27 22:20:58 +0200

    Initial commit

---
+++ src/app/components/active-label.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface ActiveLabelProps {
+  children: React.ReactNode;
+}
+
+export default function ActiveLabel({ children }: ActiveLabelProps) {
+  return <span>{children}</span>;
+}
